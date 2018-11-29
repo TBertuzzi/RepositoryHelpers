c247d92ddc9f61349ba717ba79f8d5320d60774f
Thiago Bertuzzi <thiago.bertuzzi@gmail.com>
2018-11-29 16:33:14 -0200

    :hammer: Update References

--- RepositoryHelpers/ServiceRepository/HttpExtension.cs
+++ RepositoryHelpers/ServiceRepository/HttpExtension.cs
@@ -6,103 +6,101 @@ using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 
-namespace RepositoryHelpers.ServiceRepository
-{
-    public static class HttpExtension
-    {
 
-        public static async Task<HttpResponseMessage> PostAsync(this HttpClient httpClient, string address, object dto)
+public static class HttpExtension
+{
+
+    public static async Task<HttpResponseMessage> PostAsync(this HttpClient httpClient, string address, object dto)
+    {
+        var jsonRequest = JsonConvert.SerializeObject(dto);
+        var content = new StringContent(jsonRequest, Encoding.UTF8, "text/json");
+
+        return await httpClient.PostAsync(address, content);
+    }
+
+
+    public static async Task<ServiceResponse<T>> PostAsync<T>(this HttpClient httpClient, string address,
+        object dto)
+    {
+        try
         {
             var jsonRequest = JsonConvert.SerializeObject(dto);
             var content = new StringContent(jsonRequest, Encoding.UTF8, "text/json");
 
-            return await httpClient.PostAsync(address, content);
+            var response = await httpClient.PostAsync(
+                address,
+                content);
+
+            return await GetResponse<T>(response);
         }
-
-
-        public static async Task<ServiceResponse<T>> PostAsync<T>(this HttpClient httpClient, string address,
-            object dto)
+        catch (Exception ex)
         {
-            try
-            {
-                var jsonRequest = JsonConvert.SerializeObject(dto);
-                var content = new StringContent(jsonRequest, Encoding.UTF8, "text/json");
-
-                var response = await httpClient.PostAsync(
-                    address,
-                    content);
-
-                return await GetResponse<T>(response);
-            }
-            catch (Exception ex)
-            {
-                return new ServiceResponse<T>(
-                    HttpStatusCode.InternalServerError,
-                    ex);
-            }
+            return new ServiceResponse<T>(
+                HttpStatusCode.InternalServerError,
+                ex);
         }
+    }
 
-        public static async Task<ServiceResponse<T>> GetAsync<T>(this HttpClient httpClient, string address)
+    public static async Task<ServiceResponse<T>> GetAsync<T>(this HttpClient httpClient, string address)
+    {
+        try
         {
-            try
-            {
-                var response = await httpClient.GetAsync(address).ConfigureAwait(false);
-                return await GetResponse<T>(response);
-            }
-            catch (Exception ex)
-            {
-                return new ServiceResponse<T>(
-                    HttpStatusCode.InternalServerError,
-                    ex);
-            }
+            var response = await httpClient.GetAsync(address).ConfigureAwait(false);
+            return await GetResponse<T>(response);
         }
-
-        public static async Task<ServiceResponse<T>> GetAsync<T>(this HttpClient httpClient, string address,
-            Dictionary<string, string> values)
+        catch (Exception ex)
         {
-            try
-            {
-                var builder = new StringBuilder();
-
-                foreach (var pair in values)
-                {
-                    builder.Append($"&{pair.Key}={pair.Value}");
-                }
-
-                var url = $"{address}?{builder.ToString().Substring(1)}";
-                var response = await httpClient.GetAsync(url).ConfigureAwait(false);
-                return await GetResponse<T>(response);
-            }
-            catch (Exception ex)
-            {
-                return new ServiceResponse<T>(
-                    HttpStatusCode.InternalServerError,
-                    ex);
-            }
+            return new ServiceResponse<T>(
+                HttpStatusCode.InternalServerError,
+                ex);
         }
+    }
 
-        private static async Task<ServiceResponse<T>> GetResponse<T>(HttpResponseMessage response)
+    public static async Task<ServiceResponse<T>> GetAsync<T>(this HttpClient httpClient, string address,
+        Dictionary<string, string> values)
+    {
+        try
         {
-            var returnResponse = new ServiceResponse<T>(response.StatusCode);
+            var builder = new StringBuilder();
 
-            try
+            foreach (var pair in values)
             {
-                returnResponse.Content = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
-
-                if (!response.IsSuccessStatusCode)
-                {
-                    return returnResponse;
-                }
-
-
-                returnResponse.Value = JsonConvert.DeserializeObject<T>(returnResponse.Content);
-            }
-            catch (Exception ex)
-            {
-                returnResponse.Error = ex;
+                builder.Append($"&{pair.Key}={pair.Value}");
             }
 
-            return returnResponse;
+            var url = $"{address}?{builder.ToString().Substring(1)}";
+            var response = await httpClient.GetAsync(url).ConfigureAwait(false);
+            return await GetResponse<T>(response);
         }
+        catch (Exception ex)
+        {
+            return new ServiceResponse<T>(
+                HttpStatusCode.InternalServerError,
+                ex);
+        }
+    }
+
+    private static async Task<ServiceResponse<T>> GetResponse<T>(HttpResponseMessage response)
+    {
+        var returnResponse = new ServiceResponse<T>(response.StatusCode);
+
+        try
+        {
+            returnResponse.Content = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                return returnResponse;
+            }
+
+
+            returnResponse.Value = JsonConvert.DeserializeObject<T>(returnResponse.Content);
+        }
+        catch (Exception ex)
+        {
+            returnResponse.Error = ex;
+        }
+
+        return returnResponse;
     }
 }
--- RepositoryHelpers/ServiceRepository/ServiceResponse.cs
+++ RepositoryHelpers/ServiceRepository/ServiceResponse.cs
@@ -1,58 +1,56 @@
 using System;
 using System.Net;
 
-namespace RepositoryHelpers.ServiceRepository
+
+public class ServiceResponse<T>
 {
-    public class ServiceResponse<T>
+    /// <summary>
+    /// Gets the status code.
+    /// </summary>
+    public HttpStatusCode StatusCode { get; private set; }
+
+    /// <summary>
+    /// Gets the value.
+    /// </summary>
+    public T Value { get; set; }
+
+    /// <summary>
+    /// Gets the content.
+    /// </summary>
+    public string Content { get; set; }
+
+    /// <summary>
+    /// Gets the error.
+    /// </summary>
+    public Exception Error { get; set; }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ServiceResponse{T}"/> class.
+    /// </summary>
+    /// <param name="value">
+    /// The value.
+    /// </param>
+    /// <param name="statusCode">
+    /// The status code.
+    /// </param>
+    public ServiceResponse(T value, HttpStatusCode statusCode)
     {
-        /// <summary>
-        /// Gets the status code.
-        /// </summary>
-        public HttpStatusCode StatusCode { get; private set; }
+        this.Value = value;
+        this.StatusCode = statusCode;
+    }
 
-        /// <summary>
-        /// Gets the value.
-        /// </summary>
-        public T Value { get; set; }
-
-        /// <summary>
-        /// Gets the content.
-        /// </summary>
-        public string Content { get; set; }
-
-        /// <summary>
-        /// Gets the error.
-        /// </summary>
-        public Exception Error { get; set; }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="ServiceResponse{T}"/> class.
-        /// </summary>
-        /// <param name="value">
-        /// The value.
-        /// </param>
-        /// <param name="statusCode">
-        /// The status code.
-        /// </param>
-        public ServiceResponse (T value, HttpStatusCode statusCode)
-        {
-            this.Value = value;
-            this.StatusCode = statusCode;
-        }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="ServiceResponse{T}"/> class.
-        /// </summary>
-        /// <param name="statusCode">
-        /// The status code.
-        /// </param>
-        /// <param name="error">
-        /// The error.
-        /// </param>
-        public ServiceResponse (HttpStatusCode statusCode, Exception error = null)
-        {
-            this.StatusCode = statusCode;
-            this.Error = error;
-        }
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ServiceResponse{T}"/> class.
+    /// </summary>
+    /// <param name="statusCode">
+    /// The status code.
+    /// </param>
+    /// <param name="error">
+    /// The error.
+    /// </param>
+    public ServiceResponse(HttpStatusCode statusCode, Exception error = null)
+    {
+        this.StatusCode = statusCode;
+        this.Error = error;
     }
 }
