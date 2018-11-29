60558806516d00e00ff804543a45ac8795fcc7cf
Thiago Bertuzzi <thiago.bertuzzi@gmail.com>
2018-11-29 11:43:15 -0200

    :sparkles: Add sample and Icon

---
+++ RepositoryHelpersSample/RepositoryHelpersSample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RepositoryHelpersSample
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
